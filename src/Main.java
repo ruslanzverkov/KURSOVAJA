08cdb540a71b5da0fbbfbb28a766e976603e2259
ruslanzverkov <createmed@mail.ru>
2022-08-22 18:56:21 +0300

    2

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,20 @@
 public class Main {
+    static  Employee [] employees= new Employee[10];
+
 
     public static void main(String[] args) {
+        employees[1]=new Employee("Василий Васильевич Корнев",2,20350);
+        employees[3]=new Employee("Сергей Сергевич Безруков",4,30555);
+        employees[5]=new Employee("Андрей Андреевич Сусликов",5,25500);
+        employees[6]=new Employee("Александр Александрович", 3, 23330);
+        employees[9]=new Employee("Виктория Викторовна КОшкина",1,24000);
 
-        Employee vasja=new Employee("Вася",2,20);
-        System.out.println(vasja);
+        for (int i = 0; i < employees.length; i++) {
+            if (employees[i]!=null) {
+                System.out.println(employees[i]);
+            }
+        }
+
+
     }
 }
