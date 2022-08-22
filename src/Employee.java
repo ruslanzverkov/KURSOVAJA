8e05734a7e8fba12ed96c4ab5d7eff98d65d5d8c
ruslanzverkov <createmed@mail.ru>
2022-08-22 18:33:32 +0300

    1

---
+++ src/Employee.java
@@ -0,0 +1,66 @@
+import java.util.Objects;
+
+public class Employee {
+    private int id;
+    private String fullName;
+    private int department;
+    private double salary;
+    private static int counter;
+
+    public Employee(String fullName, int department, double salary) {
+        this.fullName = fullName;
+        this.department = department;
+        this.salary = salary;
+        id=++counter;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public int getDepartment() {
+        return department;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public static int getCounter() {
+        return counter;
+    }
+
+    public void setDepartment(int department) {
+        this.department = department;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "fullName='" + fullName + '\'' +
+                ", department=" + department +
+                ", salary=" + salary +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return department == employee.department && Double.compare(employee.salary, salary) == 0 && fullName.equals(employee.fullName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(fullName, department, salary);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
-    
+
     public static void main(String[] args) {
 
+        Employee vasja=new Employee("Вася",2,20);
+        System.out.println(vasja);
     }
 }
